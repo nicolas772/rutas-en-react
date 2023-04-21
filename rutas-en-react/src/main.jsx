c11960b1af68767f6407935d469e9370ee9dd05f
Nicolas Araya Urrutia <nicolas.arayau@sansano.usm.cl>
2023-04-21 18:32:40 -0400

    implementacion de app para agregar y eliminar usuarios

--- rutas-en-react/src/main.jsx
+++ rutas-en-react/src/main.jsx
@@ -1,17 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Root from './routes/root';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import Root, { loader as rootLoader, action as rootAction} from './routes/root';
+import ErrorPage from './error-page';
+import Contact, { loader as contactLoader } from './routes/contact';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import EditContact, { action as editAction } from './routes/edit';
+import { action as destroyAction } from "./routes/destroy"
+import Index from './routes/index';
 import './index.css'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
-  },
+    errorElement: <ErrorPage></ErrorPage>,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      { index: true, element: <Index></Index>},
+      {
+        path: "contacts/:contactId",
+        element: <Contact></Contact>,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact></EditContact>,
+        loader: contactLoader,
+        action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>
+      }
+    ]
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
