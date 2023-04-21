4167cd6f4206c3ca06e6995194c80b25f8915feb
Nicolas Araya Urrutia <nicolas.arayau@sansano.usm.cl>
2023-04-21 19:32:01 -0400

    manejo de errores

--- rutas-en-react/src/main.jsx
+++ rutas-en-react/src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Root, { loader as rootLoader, action as rootAction} from './routes/root';
+import Root, { loader as rootLoader, action as rootAction } from './routes/root';
 import ErrorPage from './error-page';
-import Contact, { loader as contactLoader } from './routes/contact';
+import Contact, { loader as contactLoader, action as contactAction } from './routes/contact';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import EditContact, { action as editAction } from './routes/edit';
 import { action as destroyAction } from "./routes/destroy"
@@ -17,22 +17,28 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
-      { index: true, element: <Index></Index>},
       {
-        path: "contacts/:contactId",
-        element: <Contact></Contact>,
-        loader: contactLoader,
-      },
-      {
-        path: "contacts/:contactId/edit",
-        element: <EditContact></EditContact>,
-        loader: contactLoader,
-        action: editAction,
-      },
-      {
-        path: "contacts/:contactId/destroy",
-        action: destroyAction,
-        errorElement: <div>Oops! There was an error.</div>
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+          { index: true, element: <Index></Index> },
+          {
+            path: "contacts/:contactId",
+            element: <Contact></Contact>,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact></EditContact>,
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
+            errorElement: <div>Oops! There was an error.</div>
+          }
+        ]
       }
     ]
   }
